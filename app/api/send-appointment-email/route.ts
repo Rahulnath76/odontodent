c81cbc14f8722a8c2458c880c90f8fa2cac31ce0
Rahul Nath <nathrahul563@gmail.com>
2025-12-24 23:56:26 +0530

    minor fix

--- app/api/send-appointment-email/route.ts
+++ app/api/send-appointment-email/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const body = request.json();
+    const body = await request.json();
 
     const {
       userEmail,
